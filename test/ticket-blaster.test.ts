7f468ee6968fea6751fbd74f55748b79da70baf7
piotr.westphal <piotr.westphal@gmail.com>
2023-04-05 20:23:43 +0200

    init

---
+++ test/ticket-blaster.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TicketBlaster from '../lib/ticket-blaster-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ticket-blaster-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TicketBlaster.TicketBlasterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
